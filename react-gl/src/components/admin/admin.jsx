f6c6970760e5eb58102c4bc8ef65134e25085386
MezSpire <mimi.04.05.2003@gmail.com>
2024-01-28 01:30:28 +0100

    admin panel almost done (there is error in accepte)

--- react-gl/src/components/admin/admin.jsx
+++ react-gl/src/components/admin/admin.jsx
@@ -1,16 +1,19 @@
 import "./admin.css"
-import { useState , useEffect } from "react";
+import React, { useState , useEffect } from "react";
 import ImageModal from './image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCheck, faXmark} from '@fortawesome/free-solid-svg-icons';
+import axios from "axios";
+import {NavLink} from "react-router-dom";
+import start from "../../images/star (1).png";
 
-const ProfilePic = () => {
+const ProfilePic = ({url}) => {
     const [imageSrc, setImageSrc] = useState('./images/profile.png'); 
   
     return (
       <div style={{ position: 'relative', width: '70px', height: '70px', borderRadius: '50%', overflow: 'hidden'}}>
         <img
-          src={imageSrc}
+          src={url}
           alt="profilepic"
           style={{ width: '100%', height: '100%', }}
         />
@@ -18,50 +21,82 @@ const ProfilePic = () => {
       </div>
     );
   };
-const Accepte = () => {
+const Accepte = ({avocat,handleGetRequest}) => {
     const [carteid, setCarteid] = useState(''); 
     const [carteprof, setCarteprof] = useState('');
+    const reject = async () => {
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/admin/delete', avocat);
+
+            console.log('Response:', response.data);
+        } catch (error) {
+            console.error('Error delete:', error);
+        }
+        handleGetRequest()
+    };
     return (
       <div className="accept">
-        <ProfilePic/>
-         <div className="username">safia bennacer</div>
-         <div className="contactt">s_bennacer@estin.dz</div>
-         <div className="date">09/12/2023</div>
-         <ImageModal
-            iconSrc="./images/preview.png"
-            imageSrc={carteid}
-        />
-         <ImageModal
-            iconSrc="./images/preview.png"
-            imageSrc={carteprof}
-        />
+          <ProfilePic url={`http://127.0.0.1:8000${avocat.profilePh}`}/>
+          <div className="username">{avocat.nom} {avocat.prenom}</div>
+          <div className="contactt">{avocat.email}</div>
+          <div className="date">09/12/2023</div>
+          <ImageModal
+              iconSrc="./images/preview.png"
+              imageSrc={`http://127.0.0.1:8000${avocat.cartePro}`}
+          />
+          <ImageModal
+              iconSrc="./images/preview.png"
+              imageSrc={`http://127.0.0.1:8000${avocat.carteIndent}`}
+          />
         <div className="button">
-        <button><FontAwesomeIcon icon={faXmark} title="Delete" size="2xl" style={{color: "#f90101",cursor:'pointer'}} /></button>
+        <button onClick={reject}><FontAwesomeIcon icon={faXmark} title="Delete" size="2xl" style={{color: "#f90101",cursor:'pointer'}} /></button>
         </div>
       </div>
     );
   };
   
-  const Attente = () => {
+  const Attente = ({ avocat,handleGetRequest }) => {
     const [carteid, setCarteid] = useState('./images/profilepic.jpg'); 
     const [carteprof, setCarteprof] = useState('./images/profilepic.jpg');
+      const accept = async () => {
+          try {
+              avocat.status='active'
+              console.log('Response:', avocat);
+              const response = await axios.post('http://127.0.0.1:8000/api/admin/updatestatus', avocat);
+
+
+          } catch (error) {
+              console.error('Error update:', error);
+          }
+          handleGetRequest()
+      };
+      const reject = async () => {
+          try {
+              const response = await axios.post('http://127.0.0.1:8000/api/admin/delete', avocat);
+
+              console.log('Response:', response.data);
+          } catch (error) {
+              console.error('Error delete:', error);
+          }
+          handleGetRequest()
+      };
     return (
       <div className="attente">
-         <ProfilePic/>
-         <div className="username">safia bennacer</div>
-         <div className="contactt">s_bennacer@estin.dz</div>
+         <ProfilePic url={`http://127.0.0.1:8000${avocat.profilePh}`}/>
+         <div className="username">{avocat.nom} {avocat.prenom}</div>
+         <div className="contactt">{avocat.email}</div>
          <div className="date">09/12/2023</div>
          <ImageModal
             iconSrc="./images/preview.png"
-            imageSrc={carteid}
+            imageSrc={`http://127.0.0.1:8000${avocat.cartePro}`}
         />
          <ImageModal
             iconSrc="./images/preview.png"
-            imageSrc={carteprof}
+            imageSrc={`http://127.0.0.1:8000${avocat.carteIndent}`}
         />
         <div className="buttons">
-        <button><FontAwesomeIcon icon={faCheck} title="Accept" size="2xl" style={{color: "#0fc502",cursor:'pointer'}} /></button>
-        <button><FontAwesomeIcon icon={faXmark} title="Reject" size="2xl" style={{color: "#f90101",cursor:'pointer'}} /></button>
+        <button><FontAwesomeIcon icon={faCheck} title="Accept" size="2xl" style={{color: "#0fc502",cursor:'pointer'}} onClick={accept}/></button>
+        <button><FontAwesomeIcon icon={faXmark} title="Reject" size="2xl" style={{color: "#f90101",cursor:'pointer'}} onClick={reject}/></button>
         </div>
         
       </div>
@@ -81,8 +116,36 @@ const Admin = () => {
         setSelectedTab(tabParam);
         }
     }, []);
+    const [listAvocat, setListAvocat] = useState([]);
+    const handleGetRequest = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/admin/getPending');
+            setListAvocat(response.data);
+            console.log('Response:', response.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+    useEffect(() => {
+        handleGetRequest();
+    }, []);
 
 
+    const [listActive, setListActive] = useState([]);
+    const getActive = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/user/allAvocats');
+            setListActive(response.data);
+            console.log('Response:', response.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    useEffect(() => {
+
+        getActive();
+    }, []);
     return(
         <div className="admin">
             <div className="head">
@@ -111,11 +174,20 @@ const Admin = () => {
                 </div>
                 <div className="second">
                     {selectedTab === 'attente' && (
-                        <div><Attente/><Attente/></div>
+                        <div>
+                            {listAvocat.map((avocat, index) => (
+                                <Attente avocat={avocat} handleGetRequest={handleGetRequest}  />
+                            ))}
+                        </div>
                      
                      )}
                     {selectedTab === 'accepte' && (
-                        <div><Accepte/><Accepte/><Accepte/></div>
+                        <div>
+                            {listActive.map((avocat, index) => (
+                                <Accepte avocat={avocat} handleGetRequest={getActive}/>
+                            ))}
+
+                           </div>
                     )
                     }
                 </div>
